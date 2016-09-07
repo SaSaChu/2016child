b341a86568debe9a18e78bcbef6ac2cd9e3082d9
OA Wu <comdan66@gmail.com>
2016-09-07 15:33:43 +0800

    Minify js、html, fix gh-pages path bug.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2016 OA Wu Design
- */
-
-$(function () {
-  $('.skip').click (function () {
-    $('.animation_frame').addClass ('skip');
-  });
-  setTimeout (function () {
-    $('.animation_frame').addClass ('skip');
-  }, 4000);
-});
+$(function(){$(".skip").click(function(){$(".animation_frame").addClass("skip")}),setTimeout(function(){$(".animation_frame").addClass("skip")},4e3)});
